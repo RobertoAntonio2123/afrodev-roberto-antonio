1cab7bb1b4f629089a58659ddc6b6f3270c538ad
Roberto <robertoantonio_2015@hotmail.com>
2021-10-26 03:32:20 -0300

    parte UI

--- main.py
+++ main.py
@@ -1,16 +1,100 @@
-import numpy as np
+import sys
+import untitled
+from tables import *
 import pandas as pd
-import matplotlib.pyplot as plt
-dt = pd.read_excel('data/popularsports.xlsx')
-y  = dt['Male']
-x1 = dt['initials']
-x  = dt['Male']
+import numpy as np
 
-#plt.plot(x1, x, label='feminino')
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from tables import *
 
-plt.title("Quantidade De Atletas Em Cada Modalidade  ")
-plt.plot(x1, y,  label='(a) Athletics\n''(b) Basketball\n''(c) Football\n''(d) Judo\n'
-         '(e) Rowing\n''(f)  Rugby Sevens\n''(g) Sailing\n''(h) Shooting\n''(i)  Swimming\n')
-plt.legend()
-plt.show()
+class final_result(QMainWindow,untitled.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        super().setupUi(self)
+ #link dos botões
+        self.totalAtleta.clicked.connect(self.x1)
+        self.totalPaticipates.clicked.connect(self.x2)
+        self.totalFeminino.clicked.connect(self.x3)
+        self.totalMedalha.clicked.connect(self.x4)
+        self.totalMedalhapa.clicked.connect(self.x5)
+        self.ranking.clicked.connect(self.x6)
+        self.paisMais.clicked.connect(self.x7)
+        self.paisMenos.clicked.connect(self.x8)
+        self.quantiEsport.clicked.connect(self.x9)
+        self.timePais.clicked.connect(self.x10)
+        self.totalTreina.clicked.connect(self.x11)
+        self.quantidadeTime.clicked.connect(self.x12)
+        self.totalAtletaa.clicked.connect(self.x13)
 
+        # botão
+    def x1(self):
+        self.labelRetorno.setText(
+            str(total_participating_participants())
+        )
+
+    def x2(self):
+        self.labelRetorno.setText(
+            str(total_male_participants())
+        )
+
+    def x3(self):
+        self.labelRetorno.setText(
+            str(total_female_participants())
+        )
+
+    def x4(self):
+        self.labelRetorno.setText(
+            str(total_participants_by_sport())
+        )
+
+    def x5(self):
+        self.labelRetorno.setText(
+            str(total_medals_by_country())
+        )
+
+    def x6(self):
+        self.labelRetorno.setText(
+            str(ranking_by_total_medals())
+        )
+
+    def x7(self):
+        self.labelRetorno.setText(
+            str(The_countries_that_will_have_the_most_medals())
+        )
+
+    def x8(self):
+        self.labelRetorno.setText(
+            str(country_with_fewer_medals())
+        )
+
+    def x9(self):
+        self.labelRetorno.setText(
+            str(list_with_participating_sports())
+        )
+
+    def x11(self):
+        self.labelRetorno.setText(
+            str(number_of_coaches_per_sport())
+        )
+
+    def x12(self):
+        self.labelRetorno.setText(
+            str(how_wany_times_per_sport_each_country_has())
+        )
+
+    def x13(self):
+        self.labelRetorno.setText(
+            str(Type_of_sport_for_male_and_female())
+        )
+
+    def x10(self):
+        self.labelRetorno.setText(
+            str(number_of_coaches_per_country())
+        )
+
+
+if __name__ == '__main__':
+    qt = QApplication(sys.argv)
+    gera = final_result()
+    gera.show()
+    qt.exec_()
--- tables.py
+++ tables.py
@@ -1,14 +1,130 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-dt = pd.read_excel('data/popularsports.xlsx')
-y  = dt['Male']
-x1 = dt['initials']
-x  = dt['Male']
 
-#plt.plot(x1, x, label='feminino')
 
-plt.title("Quantidade De Atletas Em Cada Modalidade  ")
-plt.plot(x1, y,  label='(a) Athletics\n''(b) Basketball\n''(c) Football\n''(d) Judo\n'
-         '(e) Rowing\n''(f)  Rugby Sevens\n''(g) Sailing\n''(h) Shooting\n''(i)  Swimming\n')
-plt.legend()
-plt.show()
+def total_participating_participants():
+
+    """
+    Total de participantes participantes.
+    :return: class 'numpy.int64'
+    """
+    df = pd.read_excel('data/Athletes.xlsx')
+    return df.describe()['Name']['count']
+
+
+def total_male_participants():
+    """
+    Total de participantes homens.
+    :return: lass 'numpy.int64
+    """
+    dt = pd.read_excel('data/EntriesGender.xlsx')
+    return dt['Male'].sum()
+
+
+def total_female_participants():
+    """
+    Total de participantes mulheres.
+    :return:class 'numpy.int64
+    """
+    dp = pd.read_excel('data/EntriesGender.xlsx')
+    return dp['Female'].sum()
+
+
+def total_participants_by_sport():
+    """
+    Total de participantes por esporte.
+    :return: pandas.core.series.Series
+    """
+    dt = pd.read_excel('data/Athletes.xlsx')
+    return dt['Discipline'].value_counts()
+
+
+def total_medals_by_country():
+    """
+    Total de medalhas por país.
+    :return:class 'pandas.core.frame.DataFrame'
+    """
+    dt = pd.read_excel('data/Medals.xlsx')
+    dt['total'] = dt['Gold'] + dt['Silver'] + dt['Bronze']
+    return dt[['Team/NOC', 'total']]
+
+
+def ranking_by_total_medals():
+    """
+    Ranking por medalhas totais.
+    :return:class 'pandas.core.frame.DataFrame'
+    """
+    df = pd.read_excel('data/Medals.xlsx')
+    df.set_index('Rank', inplace=True)
+    df['total'] = df['Gold'] + df['Silver'] + df['Bronze']
+    return df.loc[1:3, ['Team/NOC', 'Gold', 'Silver', 'Bronze', 'total']]
+
+
+
+def The_countries_that_will_have_the_most_medals():
+    """
+    Os paises que tiverão mais medalhas .
+
+    :return:class 'pandas.core.frame.DataFrame'
+    """
+    df = pd.read_excel('data/Medals.xlsx')
+    df.drop('Rank', inplace=True, axis=1)
+    return df.loc[1:3, ['Team/NOC', 'Gold', 'Silver', 'Bronze']]
+
+
+
+def country_with_fewer_medals():
+    """
+    País com menos medalhas de medalhas
+    :return:class 'pandas.core.frame.DataFrame'
+    """
+    df = pd.read_excel('data/Medals.xlsx')
+    df.set_index('Team/NOC', inplace=True)
+    df.drop('Rank', inplace=True, axis=1)
+    return df.sort_values('Team/NOC')
+
+
+def list_with_participating_sports():
+    """
+    Lista com esportes participantes.
+    :return:class 'pandas.core.frame.DataFrame'
+    """
+    df = pd.read_excel('data/Athletes.xlsx')
+    return df['Discipline'].value_counts()
+
+
+def number_of_coaches_per_sport():
+    """
+    Quantidade de treinadores por esporte
+    :return:<class 'pandas.core.series.Series'>
+    """
+    df = pd.read_excel('data/Coaches.xlsx')
+    df.drop(['NOC'], inplace=True, axis=1)
+    return df['Discipline'].value_counts()
+
+
+def how_wany_times_per_sport_each_country_has():
+    """
+    Quanto times por esporte cada país tem..
+    :return:<class 'pandas.core.series.Series'>
+    """
+    df = pd.read_excel('data/Athletes.xlsx')
+    df.drop(['Name'], inplace=True, axis=1)
+    return df['NOC'].value_counts()
+
+def Type_of_sport_for_male_and_female():
+    """
+    Tipo de esporte para masculino e feminino
+    :return: <class 'pandas.core.series.Series'>
+    """
+    df = pd.read_excel('data/EntriesGender.xlsx')
+    return df.sort_values('Male', ascending=False)
+
+def number_of_coaches_per_country():
+    """
+    Quantidade de treinadores por país
+    País com a maior quantidade de treinadores
+    :return: <class 'pandas.core.series.Series'>
+    """
+    df = pd.read_excel('data/Coaches.xlsx')
+    df.drop(['Discipline'], inplace=True, axis=1)
+    return df['NOC'].value_counts()
